9e5196ac7c90614a1727cc04bbfab2915c0655c7
FergusStone <jff.cochard@orange.fr>
2024-03-28 12:07:15 +0100

    <feature> : Minor change on navbar

--- client/src/Navbar.jsx
+++ client/src/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = (vars) => {
   return (
     <nav class="navbar navbar-expand-lg">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#">Navbar</a>
+        <a class="navbar-brand" href="/">Menu</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
@@ -21,17 +21,23 @@ const Navbar = (vars) => {
             ))}
             <li class="nav-item dropdown">
               <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Dropdown
+                Lieux
               </a>
               <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">Action</a></li>
-                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><a class="dropdown-item" href="/menu">Attractions</a></li>
+                <li><a class="dropdown-item" href="/menu">Commerce</a></li>
                 <li><hr class="dropdown-divider" /></li>
                 <li><a class="dropdown-item" href="#">Something else here</a></li>
               </ul>
             </li>
             <li class="nav-item">
-              <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+              <a class="nav-link" href="/missions">Missions</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/avertissements">Avertissements</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/admin">Panel Admin</a>
             </li>
           </ul>
           <form class="d-flex" role="search">
