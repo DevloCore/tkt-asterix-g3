e800f165fc2f9b84973944ef87c3d1917e4b5782
FergusStone <jff.cochard@orange.fr>
2024-04-19 16:37:18 +0200

    <features> : Frontend Boutiques

--- client/src/Boutiques.jsx
+++ client/src/Boutiques.jsx
@@ -1,37 +1,24 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './assets/attractions.css';
-import Icon from '@mdi/react';
-import { mdiHumanMaleHeightVariant, mdiHumanMaleChild, mdiArrowDownDropCircle, mdiCompassRose, mdiFilter } from '@mdi/js';
-import { UserContext } from './assets/contexts/UserContext';
 
 function Menu() {
   const [commerces, setCommerces] = useState([]);
   const [selectedCommerce, setSelectedCommerce] = useState(null);
-  const [stock, setStock] = useState(null);
-
-  const { setLoading } = useContext(UserContext);
+  const [stock, setStock] = useState([]);
 
   useEffect(() => {
-    // console.log(userCon.user);
-
     async function fetchData() {
       try {
-        setLoading(true);
-
         const commercesResponse = await axios.get('commerces');
         setCommerces(commercesResponse.data);
       } catch (error) {
         console.error('Error fetching data:', error);
       }
-      finally {
-        setLoading(false);
-      }
     }
     fetchData();
   }, []);
 
-  // Fonction pour récupérer le stock d'un commerce spécifique
   const fetchStock = async (idCommerce) => {
     try {
       const response = await axios.get(`commerces/${idCommerce}/produits`);
@@ -46,35 +33,38 @@ function Menu() {
     fetchStock(idCommerce);
   };
 
-    return (
-      <div> {/* Wrapper div for the entire component */}
-        <div>
-          <h3>Boutiques :</h3>
-          <ul className="commerces-list">
-            {commerces.map((commerce) => (
-              <li key={commerce.id} onClick={() => handleCommerceClick(commerce.id)}>
-                {commerce.id} - {commerce.libelle}
-              </li>
-            ))}
-          </ul>
-        </div>
-  
-        {selectedCommerce && (
-          <div className="popup">
-            <div className="popup-content">
-              <h3>Stock restant pour le commerce {selectedCommerce} :</h3>
-              <ul>
-                {stock && stock.map(item => (
-                  <li key={item.id_produit}>{item.libelleProduit}: {item.quantite}</li>
-                ))}
-              </ul>
-              <button onClick={() => setSelectedCommerce(null)}>Fermer</button>
+  return (
+    <div className="container">
+      <div className='d-flex justify-content-center'><h4>Boutiques</h4></div>
+      <div className="commerces-list list-group">
+        {commerces.map((commerce) => (
+          <div className='list-group-item list-group-item-action' key={commerce.id} onClick={() => handleCommerceClick(commerce.id)}>
+            {commerce.libelle}
+          </div>
+        ))}
+      </div>
+
+      {selectedCommerce && (
+        <div className="popup mt-4">
+          <div className="popup-content">
+            <div className='d-flex justify-content-center'>
+              <h4>Stock du commerce <span className='text-success fw-bold'>{selectedCommerce}</span></h4>
+            </div>
+            <div className="list-group mt-3">
+              {stock.map(item => (
+                <div className="list-group-item" key={item.id_produit}>
+                  <strong>{item.libelleProduit}:</strong> {item.quantite}
+                </div>
+              ))}
+            </div>
+            <div className='d-flex justify-content-center mt-2'>
+              <button className="btn btn-secondary" onClick={() => setSelectedCommerce(null)}>Fermer</button>
             </div>
           </div>
-        )}
-      </div>
-    );
-  }
-  
+        </div>
+      )}
+    </div>
+  );
+}
 
 export default Menu;
