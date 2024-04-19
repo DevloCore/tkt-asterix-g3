906cac484ff0e22efdb1f09a43ec5b76b9155427
FergusStone <jff.cochard@orange.fr>
2024-04-19 11:03:16 +0200

    <feature> : Gestion Avertissements

--- server/controllers/AvertissementsController.js
+++ server/controllers/AvertissementsController.js
@@ -27,3 +27,44 @@ export const getGravite = async (req, res) => {
         res.status(500).json({ message: 'Internal server error' });
     }
 };
+
+export const createAvertissement = async (req, res) => {
+    try {
+        const avertissement = {
+            ...req.body,
+            date: new Date(), // Utiliser la date actuelle
+            email_utilisateur: req.body.email_utilisateur // Assumer que l'email est envoyé dans le corps de la requête
+        };
+
+        const [id] = await db('Avertissements').insert(avertissement).returning('id');
+        const newAvertissement = await db('Avertissements').where({ id }).first();
+        res.status(201).json(newAvertissement);
+    } catch (error) {
+        console.error('Error creating avertissement:', error);
+        res.status(500).send('Internal server error');
+    }
+};
+
+
+export const updateAvertissement = async (req, res) => {
+    try {
+        const { id } = req.params;
+        await db('Avertissement').where({ id }).update(req.body);
+        const updatedAvertissement = await db('Avertissement').where({ id }).first();
+        res.json(updatedAvertissement);
+    } catch (error) {
+        console.error('Error updating avertissement:', error);
+        res.status(500).send('Internal server error');
+    }
+};
+
+export const deleteAvertissement = async (req, res) => {
+    try {
+        const { id } = req.params;
+        await db('Avertissement').where({ id }).del();
+        res.status(204).send();
+    } catch (error) {
+        console.error('Error deleting avertissement:', error);
+        res.status(500).send('Internal server error');
+    }
+};
